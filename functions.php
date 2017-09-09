f658a5efd02ec909546ea2273cb4b1f93aeb70d1
Pea <pea@misfist.com>
2017-09-09 15:10:26 -0400

    Added custom nav walker

--- functions.php
+++ functions.php
@@ -5,8 +5,8 @@
  * @link https://developer.wordpress.org/themes/basics/theme-functions/
  *
  * @package understrap
- * @subpackage littlesis
- * @since 0.1.0
+ * @subpackage pai
+ * @since 0.0.1
  */
 
 include_once get_stylesheet_directory() . '/inc/enqueue.php';
@@ -14,3 +14,4 @@ include_once get_stylesheet_directory() . '/inc/setup.php';
 include_once get_stylesheet_directory() . '/inc/extras.php';
 include_once get_stylesheet_directory() . '/inc/template-tags.php';
 include_once get_stylesheet_directory() . '/inc/jetpack.php';
+include_once get_stylesheet_directory() . '/inc/bootstrap-wp-navwalker.php';
