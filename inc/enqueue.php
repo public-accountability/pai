86c8a689ea6ea9531d550a7f608b70ab31e5211d
Pea <pea@misfist.com>
2017-10-23 18:40:28 -0400

    Added JS for custom search

--- inc/enqueue.php
+++ inc/enqueue.php
@@ -50,7 +50,6 @@ function theme_enqueue_styles() {
   // wp_enqueue_style( 'pai-styles', get_stylesheet_directory_uri() . '/css/style.min.css', array(), $the_theme->get( 'Version' ) );
   wp_enqueue_style( 'pai-dev-styles', get_stylesheet_directory_uri() . '/css/style.min.css', array(), random_int( 300, 5000 ) );
   wp_enqueue_script( 'popper-scripts', get_template_directory_uri() . '/js/popper.min.js', array(), false );
-  wp_enqueue_script( 'pai-scripts', get_stylesheet_directory_uri() . '/js/app.min.js', array(), $the_theme->get( 'Version' ), true );
-
+  wp_enqueue_script( 'pai-scripts', get_stylesheet_directory_uri() . '/js/app.min.js', array( 'jquery', 'popper-scripts' ), $the_theme->get( 'Version' ), true );
 }
 add_action( 'wp_enqueue_scripts', 'theme_enqueue_styles' );
