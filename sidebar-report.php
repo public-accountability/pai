9f65c1a97516008c674bceef4f9e260c06a380a7
Pea <pea@misfist.com>
2017-12-05 18:33:30 -0500

    Modified report sidebar to use some dynamic elements

--- sidebar-report.php
+++ sidebar-report.php
@@ -9,60 +9,48 @@
 
 <div class="col-md-3 widget-area" id="right-sidebar" role="complementary">
 
-	<?php if( function_exists( 'get_field' ) && $file = get_field( 'pdf_link' ) ) : ?>
-		<aside class="button">
-			<a href="<?php echo esc_url( $file['url'] ); ?>" class="btn btn-info" download="<?php esc_html_e( $file['filename'] ); ?>"><?php esc_html_e( 'Download PDf', 'pai' ); ?></a>
-		</aside>
-	<?php endif; ?>
+	<?php pai_jetpack_share(); ?>
 
-	<aside class="toc">
+	<div class="sticky">
 
-		<?php the_widget(
-	    'ezTOC_Widget',
-			array(
-				'title' => esc_html( 'Contents', 'pai' )
-			),
-		    array()
-		); ?>
+		<?php if( function_exists( 'get_field' ) && $file = get_field( 'pdf_link' ) ) : ?>
+			<aside class="button">
+				<a href="<?php echo esc_url( $file['url'] ); ?>" class="btn btn-info" download="<?php esc_html_e( $file['filename'] ); ?>"><?php esc_html_e( 'Download PDf', 'pai' ); ?></a>
+			</aside>
+		<?php endif; ?>
 
-	</aside>
+		<?php dynamic_sidebar( 'right-sidebar' ); ?>
 
-	<aside class="related-reports">
-		<?php if( has_term( '', 'series' ) ) : ?>
-			<?php the_widget(
-		    'Series\Widgets\List_Related',
-			    array(
-						'title' => esc_html( 'Series', 'pai' )
-					),
-			    array()
-			); ?>
-		<?php else : ?>
-			<?php if( function_exists( 'get_field' ) && $content = get_field( 'related_content' ) ) : ?>
+		<?php if( function_exists( 'get_field' ) && $content = get_field( 'related_content' ) ) : ?>
+			<aside class="related-reports">
+
+					<ul class="post-list">
+
+						<?php foreach( $content as $post ) : ?>
+							<?php get_template_part( 'loop-templates/list', 'post' ) ?>
+						<?php endforeach; ?>
+
+					</ul>
+
+			</aside>
+		<?php endif; ?>
+
+		<?php if( function_exists( 'get_field' ) && $press = get_field( 'related_press' ) ) : ?>
+			<aside class="press-mentions">
+
+				<h2 class="widget-title"><?php esc_html_e( 'In the Press', 'pai' ); ?></h2>
 
 				<ul class="post-list">
 
-					<?php foreach( $content as $post ) : ?>
+					<?php foreach( $press as $post ) : ?>
 						<?php get_template_part( 'loop-templates/list', 'post' ) ?>
 					<?php endforeach; ?>
 
 				</ul>
 
-			<?php endif; ?>
+			</aside>
 		<?php endif; ?>
-	</aside>
 
-	<?php if( function_exists( 'get_field' ) && $press = get_field( 'related_press' ) ) : ?>
-		<aside class="press-mentions">
-
-			<ul class="post-list">
-
-				<?php foreach( $press as $post ) : ?>
-					<?php get_template_part( 'loop-templates/list', 'post' ) ?>
-				<?php endforeach; ?>
-
-			</ul>
-
-		</aside>
-	<?php endif; ?>
+	</div>
 
 </div><!-- #secondary -->
