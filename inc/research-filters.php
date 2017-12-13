8fac5c7b301b3031e56eb04f746d504b8259d609
Pea <pea@misfist.com>
2017-12-12 20:05:31 -0500

    Deregistered jquery slim on research page
    `$.ajax();` doesn't exist in jquery slim, so filters weren't working.

--- inc/research-filters.php
+++ inc/research-filters.php
@@ -17,11 +17,12 @@
   * @return void
   */
   function pai_research_filters_enqueue_scripts() {
-    /**
-    * frontend ajax requests.
+   /**
+    * Frontend AJAX requests.
     */
     if( is_page( 'research' ) ) {
-      wp_enqueue_script( 'pai-research-filters', get_stylesheet_directory_uri() . '/js/research-filters.js', array('jquery'), null, true );
+      wp_deregister_script( 'jquery-slim' );
+      wp_enqueue_script( 'pai-research-filters', get_stylesheet_directory_uri() . '/js/research-filters.js', array( 'jquery' ), null, true );
       wp_localize_script( 'pai-research-filters', 'research_filters',
         array(
           'nonce'         => wp_create_nonce( 'pai_research_filters' ),
@@ -89,7 +90,7 @@
       while( $posts_query->have_posts() ) :
         $posts_query->the_post();
 
-        get_template_part( 'loop-templates/content-section', 'report' );
+        get_template_part( 'loop-templates/list', 'report' );
 
       endwhile;
 
