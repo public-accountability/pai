6640dd7437bd804ac5914a46fe22e9a513c8ca5e
Pea <pea@misfist.com>
2018-02-07 22:53:37 -0500

    Updated page nav markup

--- inc/template-tags.php
+++ inc/template-tags.php
@@ -121,6 +121,7 @@ function understrap_post_nav() {
   // Don't print empty markup if there's nowhere to navigate.
   $previous = ( is_attachment() ) ? get_post( get_post()->post_parent ) : get_adjacent_post( false, '', true );
   $next     = get_adjacent_post( false, '', false );
+  $in_same_term = ( has_category( array( 'jobs' ) ) ) ? true : false;
 
   if ( ! $next && ! $previous ) {
     return;
@@ -128,16 +129,23 @@ function understrap_post_nav() {
   ?>
       <nav class="navigation post-navigation container">
         <h2 class="sr-only"><?php _e( 'Post navigation', 'understrap' ); ?></h2>
-        <div class="row nav-links justify-content-between">
-          <?php
+        <div class="row nav-links">
 
+          <div class="grid-item previous col-md-6">
+          <?php
             if ( get_previous_post_link() ) {
-              previous_post_link( '<span class="nav-previous col-md-6">%link</span>', _x( '<i class="fa fa-angle-left"></i>&nbsp;%title', 'Previous post link', 'understrap' ) );
-            }
-            if ( get_next_post_link() ) {
-              next_post_link( '<span class="nav-next col-md-6">%link</span>',     _x( '%title&nbsp;<i class="fa fa-angle-right"></i>', 'Next post link', 'understrap' ) );
+              previous_post_link( '<div class="nav-previous">%link</div>', _x( '<i class="fa fa-caret-left"></i>&nbsp;%title', 'Previous post link', 'understrap' ), $in_same_term );
             }
           ?>
+          </div>
+          <div class="grid-item next col-md-6">
+          <?php
+            if ( get_next_post_link() ) {
+              next_post_link( '<div class="nav-next">%link</div>',     _x( '%title&nbsp;<i class="fa fa-caret-right"></i>', 'Next post link', 'understrap' ), $in_same_term );
+            }
+          ?>
+          </div>
+
         </div><!-- .nav-links -->
       </nav><!-- .navigation -->
 
