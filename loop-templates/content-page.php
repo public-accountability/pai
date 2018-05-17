ce098f2c22cee162d2c741cd0597201c19c604cd
Pea <pea@misfist.com>
2018-05-16 20:26:45 -0400

    Updated content-page.php to include secondary content field

--- loop-templates/content-page.php
+++ loop-templates/content-page.php
@@ -14,9 +14,32 @@
 
 	</header><!-- .entry-header -->
 
-	<div class="entry-content page-content">
+	<?php $secondary_content = get_field( 'secondary_content' ); ?>
 
-		<?php the_content(); ?>
+	<div class="entry-content page-content<?php echo ( function_exists( 'get_field' ) && ( !empty( $secondary_content['title'] ) || !empty( $secondary_content['content'] ) ) ) ? ' has-secondary-content' : '' ; ?>">
+
+		<div class="intro">
+
+			<?php the_content(); ?>
+
+		</div>
+
+		<?php if( function_exists( 'get_field' ) && ( !empty( $secondary_content['title'] ) || !empty( $secondary_content['content'] ) ) ) : ?>
+
+			<div class="secondary-content">
+
+				<?php if( $secondary_content['title'] ) : ?>
+					<?php printf( '<h2 class="section-heading"><span>%s</span></h2>',
+						esc_html__( $secondary_content['title'], 'pai' )
+				 	); ?>
+				<?php endif; ?>
+				
+				<?php if( $secondary_content['content'] ) : ?>
+					<?php echo apply_filters( 'the_content', $secondary_content['content'] ); ?>
+				<?php endif; ?>
+			</div>
+
+		<?php endif; ?>
 
 		<?php
 		wp_link_pages( array(
