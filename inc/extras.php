e1b697754385ab146228ba41b983e26dda137b26
Pea <pea@misfist.com>
2018-06-04 17:45:27 -0400

    Try again to override parent excerpt filters

--- inc/extras.php
+++ inc/extras.php
@@ -10,22 +10,30 @@
  */
 
  /**
-  * Adds a custom read more link to all excerpts, manually or automatically generated
+  * Removes the ... from the excerpt read more link
   *
-  * @param string $post_excerpt Posts's excerpt.
+  * @param string $more The excerpt.
   *
   * @return string
   */
- function all_excerpts_get_more_link( $post_excerpt ) {
-   return $post_excerpt;
+ function custom_excerpt_more( $more ) {
+   return '';
  }
- add_filter( 'wp_trim_excerpt', 'all_excerpts_get_more_link' );
+ add_filter( 'excerpt_more', 'custom_excerpt_more' );
 
-function custom_excerpt_more( $more ) {
-	return '...<p> <a class="btn btn-secondary understrap-read-more-link" href="' . get_permalink( get_the_ID() ) . '">' . __( 'Read More <span>&rarr;</span>',
-  'understrap' ) . '</a></p>';
+/**
+ * Adds a custom read more link to all excerpts, manually or automatically generated
+ *
+ * @param string $post_excerpt Posts's excerpt.
+ *
+ * @return string
+ */
+function all_excerpts_get_more_link( $post_excerpt ) {
+
+  return $post_excerpt . '<p><a class="btn btn-secondary understrap-read-more-link" href="' . get_permalink( get_the_ID() ) . '">' . __( 'Read More <span>&rarr;</span>',
+  'pai' ) . '</a></p>';
 }
-add_filter( 'excerpt_more', 'custom_excerpt_more' );
+add_filter( 'wp_trim_excerpt', 'all_excerpts_get_more_link' );
 
 /**
  * Modify Default Archive title
