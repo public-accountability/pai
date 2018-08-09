ba5f50aa58fb350601b32e3bf3d5dfcc5c81f1aa
Pea <pea@misfist.com>
2018-08-09 18:17:30 -0400

    Added shortcode for donation form.
    Usage: `[embed-donation-form]`

--- inc/shortcodes.php
+++ inc/shortcodes.php
@@ -27,16 +27,15 @@ function pai_actionnetwork_form( $atts ) {
  		'signup-form'
  	);
 
-  $output = sprintf( '<form id="pai-signup-form" action="%s" accept-charset="UTF-8" method="post">
+  $output = sprintf( '<form id="pai-signup-form" action="%1$s" accept-charset="UTF-8" method="post">
       <input name="very_important_wink_wink" id="very_important_wink_wink" type="text" class="hidden">
       <div class="pai-signup-form-input-group">
-        <label for="email">%s</label>
-        <input name="email" id="pai-signup-form-email" placeholder="%s" required="required" type="email">
-        <input id="pai-signup-submit-button" type="submit" value="%s">
+        <label for="email">%2$s</label>
+        <input name="email" id="pai-signup-form-email" placeholder="%2$s" required="required" type="email">
+        <input id="pai-signup-submit-button" type="submit" value="%3$s">
       </div>
     </form>',
     esc_url( 'https://littlesis.org/home/pai_signup' ),
-    esc_attr__( 'Email', 'pai' ),
     esc_attr__( 'Email', 'pai' ),
     esc_attr__( 'Sign Up', 'pai' )
   );
@@ -45,3 +44,32 @@ function pai_actionnetwork_form( $atts ) {
 
  }
 add_shortcode( 'signup-form', 'pai_actionnetwork_form' );
+
+/**
+ * Shortcode to Display Donation Form
+ *
+ * @usage `[embed-donation-form]`
+ *
+ * @param  array $atts
+ * @return string $output
+ */
+function pai_actionnetwork_donation( $atts ) {
+
+	$atts = shortcode_atts(
+		array(
+			'form' => 'support-the-public-accountability-initiative-and-littlesisorg',
+		),
+		$atts,
+		'embed-donation-form'
+	);
+
+	$output = sprintf( '<script src="%1$s%2$s?format=js&source=widget"></script>
+	  <div id="can-fundraising-area-%2$s" style="width: 100%"></div>',
+    esc_url( 'https://actionnetwork.org/widgets/v2/fundraising/' ),
+    esc_attr( $atts['form'] )
+  );
+
+	return $output;
+
+}
+add_shortcode( 'embed-donation-form', 'pai_actionnetwork_donation' );
